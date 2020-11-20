2985a59216714152c015f665bae79d665fdd4d3f
XinyuYan <xinyuyan605@gmail.com>
2020-11-20 13:49:41 +0800

    complete phase 1-2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # trello-like-app
+
+URL: https://xinyu605.github.io/trello-like-app/
--- trello/src/App.js
+++ trello/src/App.js
@@ -1,37 +1,58 @@
 import React from "react";
 import "./App.css";
 
-class AddList extends React.Component {
+class InputForm extends React.Component {
   state = {
     isEditing: false,
+    value: "",
   };
-  clickAddList = () => {
+
+  handleChange = (event) => {
+    this.setState({ value: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.addTask(this.state.value);
+    this.setState({ value: "" });
+  };
+
+  handleOpen = () => {
     this.setState({ isEditing: true });
   };
 
-  clickClose = () => {
-    this.setState({ isEditing: true });
+  handleClose = () => {
+    this.setState({ isEditing: false });
   };
 
   render() {
     if (this.state.isEditing === false) {
       return (
-        <form>
-          <h2>Phase 1-1</h2>
-          <button className="button" id="addBtn" onClick={this.clickAddList}>
-            Add List
+        <form onSubmit={this.handleSubmit}>
+          <h2>Phase 1-2</h2>
+          <button className="button" onClick={this.handleOpen}>
+            + Add List
+          </button>
+          <button className="button" onClick={this.handleClose}>
+            X
           </button>
         </form>
       );
     } else {
       return (
-        <form>
-          <h2>Phase 1-1</h2>
-          <input type="text" id="inputContent"></input>
-          <button className="button" id="addBtn">
-            Add List
+        <form onSubmit={this.handleSubmit}>
+          <h2>Phase 1-2</h2>
+          <input
+            type="text"
+            id="newTodoInput"
+            className="inputText"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+          <button type="submit" className="button">
+            + Add List
           </button>
-          <button className="button" id="closeButton" onClick={this.clickClose}>
+          <button className="button" onClick={this.handleClose}>
             X
           </button>
         </form>
@@ -40,18 +61,55 @@ class AddList extends React.Component {
   }
 }
 
+const Todo = (props) => {
+  // console.log(props);
+  return (
+    <li className="todo">
+      <div className="boxWraper">
+        <label>{props.name}</label>
+      </div>
+      <div className="btnGroup"></div>
+    </li>
+  );
+};
+
+// function App() {
 class App extends React.Component {
-  // state = {
-  //   isEditing: false,
-  // };
-  // clickAddList = (event) => {
-  //   this.setState({ pendingValue: event.currentTarget.value });
-  // };
+  state = {
+    data: [],
+  };
+
+  handleAddTask = (content) => {
+    console.log(content);
+    let newData = this.state.data;
+    // console.log(newData);
+    newData.push({
+      id: this.state.data.length,
+      name: content,
+      completed: false,
+    });
+    // console.log(newData);
+    this.setState(newData);
+    // console.log(this.state);
+  };
 
   render() {
+    console.log(this.state);
+    // console.log(this.state.data);
     return (
       <div className="App">
-        <AddList />
+        <InputForm addTask={this.handleAddTask} />
+
+        <ul className="todoList">
+          {this.state.data.map((item) => (
+            <Todo
+              name={item.name}
+              completed={item.completed}
+              id={item.id}
+              key={item.id}
+            />
+          ))}
+        </ul>
       </div>
     );
   }
