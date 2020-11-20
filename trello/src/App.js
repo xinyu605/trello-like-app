1698b0f96de86da52f7b69bac6f8f10b427fe5a7
XinyuYan <xinyuyan605@gmail.com>
2020-11-20 11:02:19 +0800

    complete phase 1-1

--- trello/src/App.js
+++ trello/src/App.js
@@ -1,25 +1,60 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class AddList extends React.Component {
+  state = {
+    isEditing: false,
+  };
+  clickAddList = () => {
+    this.setState({ isEditing: true });
+  };
+
+  clickClose = () => {
+    this.setState({ isEditing: true });
+  };
+
+  render() {
+    if (this.state.isEditing === false) {
+      return (
+        <form>
+          <h2>Phase 1-1</h2>
+          <button className="button" id="addBtn" onClick={this.clickAddList}>
+            Add List
+          </button>
+        </form>
+      );
+    } else {
+      return (
+        <form>
+          <h2>Phase 1-1</h2>
+          <input type="text" id="inputContent"></input>
+          <button className="button" id="addBtn">
+            Add List
+          </button>
+          <button className="button" id="closeButton" onClick={this.clickClose}>
+            X
+          </button>
+        </form>
+      );
+    }
+  }
+}
+
+class App extends React.Component {
+  // state = {
+  //   isEditing: false,
+  // };
+  // clickAddList = (event) => {
+  //   this.setState({ pendingValue: event.currentTarget.value });
+  // };
+
+  render() {
+    return (
+      <div className="App">
+        <AddList />
+      </div>
+    );
+  }
 }
 
 export default App;
